e91108e549fc12c7aa9cdba67506d0c3e9aef25a
Beth McNany <beth.mcnany@gmail.com>
2012-12-19 03:11:36 -0500

    hopefully classifier works now

--- learnfeatures.py
+++ learnfeatures.py
@@ -22,9 +22,11 @@ class AveragedPerceptron:
         [[x0,x1,...,xn], y]
         where xi is +/-1 feature value and y is +/-1 true label
         '''
-        w = [0 for i in range(len(features))]  # weights
+        nfeats = len(data[0][0])
+        
+        w = [0 for i in range(nfeats)]  # weights
         b = 0  # bias
-        u = [0 for i in range(len(features))]  # cached weights
+        u = [0 for i in range(nfeats)]  # cached weights
         B = 0  # cached bias
         c = 0  # counter
         for _ in range(maxIter):
@@ -54,26 +56,52 @@ class AveragedPerceptron:
 class Classifier:
     def __init__(self):
         self.rawfeatures = {"train":[], "test":[]}
-        self.vocabulary = {}    # holds list of all words seen in training data
-        self.data = {}          # holds data to pass to perceptron
+        self.vocabulary = {"train":[], "test":[]}    # holds list of all words seen in training data
+        self.authors = {"train":[], "test":[]}       # holds list of authors
+        self.books = {"train":[], "test":[]}
+        self.data = {"train":[], "test":[]}          # holds data to pass to perceptron
         self.classifiers = {}   # holds classifiers
-
+        
     def printstats(self):
         # print some pretty stats or something here?
         print "stats"
         
     def supervisedLearn(self):
         # using author info for labeled examples
-        if VERBOSE: print "Supervised learning i.e. neural network"
+        if VERBOSE: print "supervised learning i.e. neural network"
+        
+        # now we make all the classifiers and train them all
+        maxIter = 50
+        
+        for a in self.authors['train']:
+            if VERBOSE: print "training classifier for",a,"...",
+            self.classifiers[a] = AveragedPerceptron(a)
+            
+            # train them
+            alldata = []    # format [ [[x0,x1,...,xn], y], [...], ... ] for each book; y = +-1 author
+            for i, data in enumerate(self.data['train']):
+                isauth = 1 if a == self.authors['train'][i] else -1
+                alldata.append( [ data, isauth ] )
+            
+            self.classifiers[a].train(alldata, maxIter)
+            if VERBOSE: print "done."
+            
+        # print to file
+        if VERBOSE: print "pickling to file..."
+        f = file("classifierpickle.txt", "wb")
+        pickle.dump(self.classifiers, f)
+        f.close()
+        if VERBOSE: print "done."
 
     def unsupervisedLearn(self):
         # TODO: k-means clustering ?
         if VERBOSE: print "Unsupervised learning"
 
     def classify(self, inputTxt):
+        if VERBOSE: print "classifying"
+
         # OVA
         # AVA
-        if VERBOSE: print "classifying"
 
     def loadFeatures(self):
         for n in ["train", "test"]:
@@ -87,56 +115,57 @@ class Classifier:
                 if VERBOSE: print "done."
         
         if VERBOSE: print "\n"
+        
+        self.fixFeatures()
     
     def fixFeatures(self):
         # turn features into a nice data set
         # return data suitable for passing into AveragedPerceptronTrain
         
+        # get vocabulary list
+        vocab = []
+        for book in self.rawfeatures['train']:
+            vocab += book.vocabulary.keys()
+        
+        # TODO remove all stop words
+        for wd in ["a", "an", "the"]:
+            vocab.remove(wd)
+        
         for n, books in self.rawfeatures.iteritems():
             if VERBOSE: print "calculating %sing features..."%n,
-            vocablist = BookStats()
-            for book in books: # book = BookInfo object
-                # the worst thing space-wise will be vocabulary countss
-                # list of all words in books
-                vocablist.add(book.vocabulary)
-        
-            # TODO remove all stop words
-            for wd in ["a", "an", "the"]:
-                vocablist.pop(wd)
-            
-            # TODO we need to normalize or center this around zero? I think?
-            # actually bias should take care of this
-            self.vocabulary[n] = vocablist
             
             # self.data = {'train': [[[x0,x1,...,xn], y], [...]], 'test':...}
             self.data[n] = []
             
-            # calculate some stats about lengths
-            # min, max, average, stddev
-            for feat, vals in book.features.iteritems():    # sentence, par, etc. lengths
-                self.data[n].append(min(vals))
-                self.data[n].append(max(vals))
+            for book in books: # book = BookInfo object
+                # calculate some stats about lengths
+                # min, max, average, stddev
+                bdata = []
+                for feat, vals in book.features.iteritems():    # sentence, par, etc. lengths
+                    bdata.append(min(vals))
+                    bdata.append(max(vals))
+                    
+                    avg = sum(vals) / float(len(vals))
+                    stddev =  ( sum([(i-avg)**2 for i in vals]) / (len(vals)-1) ) ** (0.5)
+                    bdata.append(avg)
+                    bdata.append(stddev)
                 
-                avg = sum(vals) / float(len(vals))
-                stddev =  ( sum([(i-avg)**2 for i in vals]) / (len(vals)-1) ) ** (0.5)
-                self.data[n].append(avg)
-                self.data[n].append(stddev)
+                # now add vocabulary
+                for wd in vocab:
+                    bdata.append(book.vocabulary[wd])
+                
+                # now add bdata to data
+                self.data[n].append(bdata)
+                self.authors[n].append(book.author)
+                self.books[n].append(book.title)
             
             if VERBOSE: print "done."
             
-        # after that, we add vocabulary to data
-        # only add the training data vocabs so that the feature vectors are the same
-        if VERBOSE: print "\nadding vocabulary features...",
-        for word, val in self.vocabulary['train'].iteritems():
-            self.data['train'].append(val)
-            self.data['test'].append(self.vocabulary['test'][word])
-        
         if VERBOSE:
-            print "done.\n"
-            print "Training:",len(self.data['train'])
-            print "Testing: ",len(self.data['test']), "\n"
+            print "Training:",len(self.data['train'][0])
+            print "Testing: ",len(self.data['test'][0]), "\n"
 
 if __name__ == "__main__":
     c = Classifier()
     c.loadFeatures()
-    c.fixFeatures()
+    c.supervisedLearn()
